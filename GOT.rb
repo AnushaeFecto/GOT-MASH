df2e1f3aa86ea6ce7c92fe0f0ecf3a9ce249895a
Anushae <anushaefecto@gmail.com>
2017-12-14 02:21:03 +0000

    1
    1

--- GOT.rb
+++ GOT.rb
@@ -6,5 +6,5 @@ get '/' do
 end
 post '/' do
   params.to_json
-  "you will marry #{params[:hottie].sample} and pledge your loyalty to house #{params[:house].sample}, as a #{params[:job].sample}, whilst residing in #{params[:location].sample}, you will fignt #{params[:battle].sample} battles in your life time and spend your leisure time with your pet #{params[:pet].sample}. Your superpower will be #{params[:power].sample} that you will use to destroy your nemisis #{params[:enemy].sample}."
+  "you will marry #{params[:hottie].sample} and pledge your loyalty to house #{params[:house].sample}, as a #{params[:job].sample}, whilst residing in #{params[:location].sample}, you will fight #{params[:battle].sample} battles in your life time and spend your leisure time with your pet #{params[:pet].sample}. Your superpower will be #{params[:power].sample} that you will use to destroy your nemisis #{params[:enemy].sample}."
 end
